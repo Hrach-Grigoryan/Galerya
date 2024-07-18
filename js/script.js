51984866bfbf99cdafeef99e82121095c1e9f5b8
Hrach-Grigoryan <grigoryanhrach97@gmail.com>
2024-07-18 13:35:53 +0300

    galerya

---
+++ js/script.js
@@ -0,0 +1,24 @@
+'use strict'
+
+var myimg = document.getElementById('myimg')
+var myimgh = document.getElementById('myimgh')
+var myimghj = document.getElementById('myimghj')
+var myimgl = document.getElementById('myimgl')
+var myimgf = document.getElementById('myimgf')
+
+function myfunction(){
+    myimg.src = myimgh.src  
+
+}
+
+function myfunction1(){
+    myimg.src = myimghj.src
+}
+
+function myfunction2(){
+    myimg.src = myimgl.src
+}
+
+function myfunction3(){
+    myimg.src = myimgf.src
+}
